087c6a2b9ce718c661dd95e64d16db5fa6d1edd0
vitorsemeano <vitorsemeano@gmail.com>
2019-02-27 23:29:13 +0000

    module resoltion for viewContainer

--- src/scripts/site.js
+++ src/scripts/site.js
@@ -970,7 +970,7 @@ var AppInfo = {};
         define("imageUploader", [embyWebComponentsBowerPath + "/imageuploader/imageuploader"], returnFirstDependency);
         define("navdrawer", ["components/navdrawer/navdrawer"], returnFirstDependency);
         define("htmlMediaHelper", [embyWebComponentsBowerPath + "/htmlvideoplayer/htmlmediahelper"], returnFirstDependency);
-        define("viewcontainer", ["components/viewcontainer-lite"], returnFirstDependency);
+        define("viewcontainer", ["components/viewContainer"], returnFirstDependency);
         define("queryString", [bowerPath + "/query-string/index"], function () {
             return queryString;
         });
